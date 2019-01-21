34aeeae22d255883f19888140c260af34fb32552
Nikos Sandalidis <nikos.sandalidis@aurea.com>
2019-01-21 11:45:44 +0200

    Took test for Authority Partners (Bosnia) in Codility and failed all 3 tasks

---
+++ Codility/2019-01-21 Authority partners Bosnia/Test.cs
@@ -0,0 +1,71 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//namespace ConsoleApp10
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//            int[] A = { 1, 2, -5, 5, -12 };
+//            Array.Sort(A);
+
+//            var kot = solution(A);
+//        }
+
+//        public static int solution(int[] A)
+//        {
+//            var nar = new NegArray(A);
+
+//            return 0;
+//        }
+
+//        private class NegArray
+//        {
+//            private int offset;
+//            private bool[] arr;
+
+//            private static void minmax(int[] A, out int min, out int max)
+//            {
+//                min = int.MaxValue;
+//                max = int.MinValue;
+
+//                for (int i = 0; i < A.Length; i++)
+//                {
+//                    if (A[i] > max) max = A[i];
+//                    if (A[i] < min) min = A[i];
+//                }
+//            }
+
+//            public NegArray(int[] A)
+//            {
+//                int min = 0;
+//                int max = 0;
+//                minmax(A, out min, out max);
+//                int l = max - min;
+//                if (min < 0) l = max - min;
+//                l++;
+//                arr = new bool[l];
+//                offset = min;
+//                for (int i = 0; i < A.Length; i++)
+//                {
+//                    this[A[i]] = true;
+//                }
+//            }
+
+//            public bool this[int index]
+//            {
+//                get
+//                {
+//                    return arr[index - offset];
+//                }
+//                set
+//                {
+//                    var offsetIndex = index - offset;
+//                    arr[offsetIndex] = value;
+//                }
+//            }
+//        }
+//    }
+//}
