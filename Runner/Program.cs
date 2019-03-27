6c8622eaa9d33dd9df9f854bd56c3b7edf8e41a0
Nikos Sandalidis <nikos@ti-soft.com>
2019-03-27 20:38:43 +0200

    Δοκιμή επαλήθευσης πολλαπλασιαμού

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -7,15 +7,20 @@ namespace Runner
     {
         static void Main(string[] args)
         {
-            string s = null;
-            try
-            {
-                var k = s.Length;
-            }
-            catch (Exception x)
-            {
+            var test = Stelios.MultiplicationValidator.Validate(87652892, 982345);
 
-            }            
+            const int max = 1000;
+            long counter = 0;
+            long total = max * max;
+
+            for(int a = 1; a < max; a++)
+                for (int b = 1; b < max; b++)
+                {
+                    counter++;
+                    var percent = (double)counter / (double)total * (double)100;
+                    var v = Stelios.MultiplicationValidator.Validate(a, b);
+                    if (!v) throw new Exception(a.ToString() + "," + b.ToString());
+                }
         }
     }
 }
---
+++ Stelios/MultiplicationValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace Stelios
+{
+    public class MultiplicationValidator
+    {
+        public static bool Validate(long a, long b)
+        {
+            var c = checked(a * b);
+            var t1 = Reduce(a);
+            var t2 = Reduce(b);
+            var t3 = Reduce(t1 * t2);
+            var t4 = Reduce(c);
+            return t3 == t4;
+        }
+
+        private static int Reduce(long a)
+        {
+            while (a > 9)
+            {
+                long total = 0;
+                foreach(var c in a.ToString()) total += int.Parse(c.ToString());
+                a = total;
+            }
+
+            return (int)a;
+        }
+    }
+}
