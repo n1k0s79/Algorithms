dc902171c76b1743dba56ac797514a8a2fe94237
Nikos Sandalidis <nikos@ti-soft.com>
2022-06-14 13:05:43 +0300

    add codesignal algorithm

--- javascriptTests/app.js
+++ javascriptTests/app.js
@@ -1,8 +1,15 @@
 import { isPalindrome } from "./palindrome.js";
 import { getMaxProduct } from "./adjacentElementsProduct.js";
-import { getShapeArea } from "./shapeArea.js";
+import { getShapeArea, getShapeArea2 } from "./shapeArea.js";
 import { consecutive } from "./consecutive.js";
 import { strictlyIncreasing } from "./strictlyIncreasing.js";
 import { getMatrixElementsSum } from "./matrixElementsSum.js";
-const k = getMatrixElementsSum(matrix);
-const a = 1;
+
+for(var i =1; i < 15; i++) {
+    var a = getShapeArea(i);
+    var b = getShapeArea2(i);
+    var c = 1 + (i -1) * 4
+    if (a != b || a != c) {
+        var j = 1;
+    }
+}
--- javascriptTests/shapeArea.js
+++ javascriptTests/shapeArea.js
@@ -1,3 +1,20 @@
 export function getShapeArea(n) {
-    return n * n + (n-1) * (n-1);    
+    return n * n + (n-1) * (n-1);
+}
+
+var result = getShapeArea2(4);
+
+export function getShapeArea2(n){
+    var top = n * 2 - 1;
+    var total = 0;
+    var current = top;
+    for (var i = n - 1; i > 0; i--) {
+        current = current -2;
+        if (i !== 1) {
+            total = total + current;
+        } else total = total + 1;
+    }
+
+    total = total * 2 + top
+    return total;
 }
