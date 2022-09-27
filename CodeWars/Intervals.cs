b227e0341d90bf9d6a5de8b91ecfb20e4aca8710
Nikos Sandalidis <nikos@ti-soft.com>
2022-09-27 11:16:54 +0300

    Codewars intervals

---
+++ CodeWars/Intervals.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars
+{
+    [TestClass]
+    public class Intervals
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var input = new (int, int)[] { (1, 5), (10, 17) };
+            var j = SumIntervals(input);
+        }
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            var input = new (int, int)[] { (1, 2), (6, 10), (11, 15) };
+            var result = SumIntervals(input);
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var input = new (int, int)[] { (1, 4), (7, 10), (3, 5) };
+            var result = SumIntervals(input);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var input = new (int, int)[] { (1, 5), (10, 20), (1, 6), (16, 19), (5, 11) };
+            var result = SumIntervals(input);
+            Assert.AreEqual(19, result);
+        }
+
+        // Η λύση μου
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            var items = intervals.Select(i => new Interval(i)).ToList();
+            var newList = new List<Interval>();
+            foreach(var item in items)
+            {
+                var overlapping = newList.Where(i => i.GetOverlap(item) != null).ToList();
+                if (overlapping.Any())
+                {
+                    overlapping.ToList().ForEach(o => newList.Remove(o));
+                    var merged = Interval.Merge(overlapping.Concat(new Interval[] { item }).ToArray());
+                    newList.Add(merged);
+                }
+                else
+                {
+                    newList.Add(item);
+                }
+            }
+            var ret = newList.Sum(i => i.Length);
+            return ret;
+        }
+
+        [DebuggerDisplay("{From}-{To}")]
+        class Interval
+        {
+            public int From;
+            public int To;
+            public Interval(int from, int to)
+            {
+                From = from;
+                To = to;
+            }
+
+            public Interval((int, int) couple)
+            {
+                From = couple.Item1;
+                To = couple.Item2;
+            }
+
+            public int Length => To - From;
+            public Interval GetOverlap(Interval other)
+            {
+                var from = Math.Max(From, other.From);
+                var to = Math.Min(To, other.To);
+                return from > to ? null : new Interval(from, to);
+            }
+
+            public static Interval Merge(params Interval[] items) => items.Aggregate((a, b) => a.MergeWith(b));
+
+            public Interval MergeWith(Interval other)
+            {
+                var from = Math.Min(From, other.From);
+                var to = Math.Max(To, other.To);
+                return new Interval(from, to);
+            }
+
+            public static Interval GetOverlap(params Interval[] intervals) => intervals.Aggregate((a, b) => a.GetOverlap(b));
+        }
+
+        /// <summary> Αυτή τη λύση τη βρήκα στο codewars 
+        /// Πώς δουλεύει και γιατί; </summary>
+        public static int SumIntervals2((int min, int max)[] intervals)
+        {
+            var prevMax = int.MinValue;
+
+            return intervals
+                .OrderBy(x => x.min)
+                .ThenBy(x => x.max)
+                .Aggregate(0, (acc, x) => acc += prevMax < x.max ? -Math.Max(x.min, prevMax) + (prevMax = x.max) : 0);
+        }
+    }
+}
--- Math/Permutation.cs
+++ Math/Permutation.cs
@@ -12,7 +12,47 @@ namespace Math
             if (!length.HasValue) length = items.Count();
             return length == 1 ? items.Select(i => new T[] { i }) : GetAll(items, length - 1).SelectMany(l => items.Except(l), (l, i) => l.Concat(new T[] { i }));
         }
-         
+
+        /*
+         * Πώς δουλεύει η παραπάνω; Την έχω ξαναγράψει εδώ λίγο πιο ανεπτυγμένη
+         * Για Ν στοιχεία:
+         * Κάνει αναδρομή μέχρι να φτάσει να φτιάξει Ν λίστες του ενός στοιχείου
+         * π.χ. για 1, 2, 3: {1}, {2}, {3}
+         * Μετά σε κάθε βήμα της αναδρομής
+         * για κάθε μία από τις λίστες του προηγούμενου βήματος
+         * φτιάχνει μία άλλη λίστα η οποία περιέχει όλα τα στοιχεία της αρχικής λίστας ΠΛΗΝ αυτών
+         * συνενώνει και επιστρέφει τις λίστες για το επόμενο βήμα
+         */
+
+        public static IEnumerable<IEnumerable<T>> GetAllΤ<T>(IEnumerable<T> items, int? length = null)
+        {
+            if (!length.HasValue) length = items.Count();
+            if (length == 1) return items.Select(i => new T[] { i }).ToList();
+            var prevStepLists = GetAllΤ(items, length - 1).ToList();
+            var currentStepLists = prevStepLists.SelectMany(l => items.Except(l), (l, i) => l.Concat(new T[] { i }).ToList()).ToList();
+            return currentStepLists;
+        }
+
+        /// <summary> H δική μου συνάρτηση για τις μεταθέσεις. (26/9/2022) 
+        /// 20 γραμμές κώδικα αντί 2 του linq </summary>
+        public static List<List<T>> GetAllPermutations<T>(List<T> list)
+        {
+            if (list.Count == 2) return new List<List<T>>() { new List<T>() { list[0], list[1] }, new List<T>() { list[1], list[0] } };
+            var currentLists = new List<List<T>>();
+            foreach (var head in list)
+            {
+                var tail = list.Except(new T[] { head }).ToList();
+                var prevPermutations = GetAllPermutations(tail);
+                foreach (var prevPerm in prevPermutations)
+                {
+                    var newList = new List<T>() { head };
+                    newList.AddRange(prevPerm);
+                    currentLists.Add(newList);
+                }
+            }
+            return currentLists;
+        }
+
         private static List<T> GetAllExcept<T>(List<T> initial, List<T> exceptions) => initial.Except(exceptions).ToList();
 
         public static int GetNumberOfAllPossible(int n) => Factorial.GetPrecalculated(n);
